6f00078cd25c423f955ac6ffcc5f38ca5a3e1708
Matheus Giraldi <115491408+Giraldimatheus@users.noreply.github.com>
2023-03-06 15:31:19 -0300

    Add files via upload

---
+++ 7-Condicionais/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_Condicionais
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 7 - Condicionais");
+
+            int idadejoao = 18;
+            int quantidadepessoas = 1;
+            if (idadejoao >= 18)
+            {
+                Console.WriteLine("Joao tem mais de 18 anos, pode entrar.");
+            }
+            else
+            {
+                if (quantidadepessoas >= 2)
+                {
+                    Console.WriteLine("Pode entrar...");
+                }
+                else { Console.WriteLine("Não tem 18 anos e ta sozin, não pode entrar vacilao"); }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 8-Condicionais 2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_Condicionais
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 8 - Condicionais 2");
+            
+            int idadejoao = 18;
+            //int quantidadepessoas = 1
+            //bool acompanhado = quantidadepessoas >= 2;
+            bool acompanhado = true;
+
+            if (idadejoao >= 18 || acompanhado==true)
+            {
+                Console.WriteLine("Pode entrar.");
+            }
+            else
+            {
+                Console.WriteLine("Não pode entrar vacilao");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
