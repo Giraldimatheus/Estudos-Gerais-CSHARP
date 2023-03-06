fa591bbb499af0c0ca44c465bc6af9e99b8586bc
Matheus Giraldi <115491408+Giraldimatheus@users.noreply.github.com>
2023-03-06 15:34:04 -0300

    Add files via upload

---
+++ P13-ForEncadeado/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P13_ForEncadeado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Projeto 13");
+
+            //*
+            //**
+            //***
+            //****
+            //*****
+            //******
+            //*******
+
+            //para cada linha um for, para cada inclusão de * um for também.
+
+            for (int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+            {
+                for (int contadorColuna = 0; contadorColuna < 10; contadorColuna++)
+                {
+                    if (contadorColuna <= contadorLinha)
+                        Console.Write("*");
+                    else
+                        break;
+                }
+
+                Console.WriteLine();
+            }
+
+            //uma forma diferente de fazer a escrita
+
+            for (int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+            {
+                for (int contadorColuna = 0; contadorColuna <= contadorLinha; contadorColuna++)
+                {
+                        Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ p12- CalculaInvestimentoLongoPrazo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p12__CalculaInvestimentoLongoPrazo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 12");
+            double ValorInvestido = 1000;
+            double FatorRendimento = 1.0036;
+            for(int ContadorAno = 1; ContadorAno <=5; ContadorAno++)
+            {
+                for(int ContadorMes= 1; ContadorMes <=12; ContadorMes++)
+                {
+                    ValorInvestido *= FatorRendimento;
+                }
+
+
+                FatorRendimento += 0.0010;
+            }
+
+            Console.WriteLine("Ao término do investimento, você terá R$" + ValorInvestido);
+
+            Console.ReadLine();
+        }
+    }
+}
